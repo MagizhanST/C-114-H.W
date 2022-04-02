e960439b94b804df2e0eb350c9bd24e74d77ff5b
MagizhanST <89840603+MagizhanST@users.noreply.github.com>
2022-04-02 12:57:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,27 +2,13 @@ function preload(){
 }
 
 function setup(){
-    var canvas = createCanvas(640, 480);
-    canvas.parent('canvas-holder');
-    video = createCapture(VIDEO);
-    video.hide();
-
-    tint_color = "";
+    canvas = createCanvas(300,300);
+    canvas.center();
 }
 
 function draw(){
-    image(video, 0, 0, 640, 480);
-    fill('blue');
-    circle(30, 30, 40);
-    fill('red');
-    square(590, 430, 40);
-    tint(tint_color);
 }
 
-function snapshot(){
-    save('student_name.png');
-}
-
-function tint_fill(){
-    tint_color = document.getElementById("color_name").value;
+function take_snapshot(){
+    save("my_picture.png");
 }
